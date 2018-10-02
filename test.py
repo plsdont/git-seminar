4d27ed890ea4969025ff796efa00a35a15a9d33f
Raspi <tripplesniper@gmai.com>
2018-10-02 11:27:04 +0000

    cc

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
 print("tests")
 print("a")
 =======
-<<<<<<< HEAD
-print("stests")
->>>>>>> origin/master
-=======
-print("stestsssssss")
-=======
->>>>>>> 40e36404cd2314d6fd64013b0df685600a0804d2
-
